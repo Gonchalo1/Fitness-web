7d6cb9763c5aa50055a0c90a73f68a4038adf6f9
G <gonzalocal127@gmail.com>
2024-07-15 13:11:29 -0300

    Advancing with the Nav and the styles of the footer

--- src/sections/footer/footer.js
+++ src/sections/footer/footer.js
@@ -1,12 +1,119 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../App.css';
+import Logo from '../images/icon.webp';
 
 function Footer() {
-  return (
-    <div className="App">
-      Footer
-    </div>
-  );
+    return (
+        <footer className="footer">
+            <div className="footer-logo">
+                <div className='logo-container'>
+                    <img src={Logo} alt="Logo" />
+                </div>
+            </div>
+            <div className="footer-secciones">
+                <p>Sobre mi</p>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <span>¿Quien soy?</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/miembros">
+                            <span>¿Donde Encontrarme?</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/precio">
+                            <span>Distribución</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/noticias">
+                            <span>Contacto</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contacto">
+                            <span>Contacto</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer-secciones">
+                <p>Productos</p>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <span>Pantalones</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/miembros">
+                            <span>Remeras</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/precio">
+                            <span>Calzas</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/noticias">
+                            <span>Buzos</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contacto">
+                            <span>Tops</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer-secciones">
+                <p>Secciones</p>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <span>Home</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/miembros">
+                            <span>Miembros</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/precio">
+                            <span>Precio</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/noticias">
+                            <span>Noticias</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contacto">
+                            <span>Contacto</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer-creado-por">
+                <span>
+                    Creado por Gonzalo Calderón: 
+                    <a href="https://instagram.com/_gonchalo_" target="_blank" rel="noopener noreferrer"> _gonchalo_ </a>
+                    <div className="instagram-icon-container">
+                        <a href="https://www.instagram.com/melina.gomezl/" target="_blank" rel="noopener noreferrer">
+                            <i className="bi bi-instagram"></i>
+                        </a>
+                    </div>
+                </span>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/sections/nav/nav.js
+++ src/sections/nav/nav.js
@@ -3,11 +3,30 @@ import '../../App.css';
 import { Link } from 'react-router-dom';
 import Icon from '../images/icon.webp';
 
+
 function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [submenuProductsOpen, setSubmenuProductsOpen] = useState(false);
+  const [submenuPlansOpen, setSubmenuPlansOpen] = useState(false);
 
   const handleMenuToggle = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const handleProductsMouseEnter = () => {
+    setSubmenuProductsOpen(true);
+  };
+
+  const handleProductsMouseLeave = () => {
+    setSubmenuProductsOpen(false);
+  };
+
+  const handlePlansMouseEnter = () => {
+    setSubmenuPlansOpen(true);
+  };
+
+  const handlePlansMouseLeave = () => {
+    setSubmenuPlansOpen(false);
   };
 
   return (
@@ -18,13 +37,47 @@ function Nav() {
         </div>
         <div className="icon">
           <Link to='/'>
-          <img src={Icon} alt='icon'/>
+            <img src={Icon} alt='icon'/>
           </Link>
         </div>
         <ul className={`navigation-items ${menuOpen ? 'open' : ''}`}>
-          <li><Link to="/products">Productos</Link></li>
+          <li onMouseEnter={handleProductsMouseEnter} onMouseLeave={handleProductsMouseLeave}>
+            <Link to="/products">
+              Productos
+              <span className="nav-icon">
+                <i className="bi bi-chevron-down desktop-icon"></i>
+                <i className="bi bi-chevron-right mobile-icon"></i>
+              </span>
+            </Link>
+            {submenuProductsOpen && (
+              <div className="submenu" onMouseEnter={handleProductsMouseEnter} onMouseLeave={handleProductsMouseLeave}>
+                <ul>
+                  <li><Link to="/subpage1">Subpage 1</Link></li>
+                  <li><Link to="/subpage2">Subpage 2</Link></li>
+                  <li><Link to="/subpage3">Subpage 3</Link></li>
+                </ul>
+              </div>
+            )}
+          </li>
           <li><Link to="/contact">Contacto</Link></li>
-          <li><Link to="/plans">Planificaciones</Link></li>
+          <li onMouseEnter={handlePlansMouseEnter} onMouseLeave={handlePlansMouseLeave}>
+            <Link to="/plans">
+              Planificaciones
+              <span className="nav-icon">
+                <i className="bi bi-chevron-down desktop-icon"></i>
+                <i className="bi bi-chevron-right mobile-icon"></i>
+              </span>
+            </Link>
+            {submenuPlansOpen && (
+              <div className="submenu" onMouseEnter={handlePlansMouseEnter} onMouseLeave={handlePlansMouseLeave}>
+                <ul>
+                  <li><Link to="/plan1">Plan 1</Link></li>
+                  <li><Link to="/plan2">Plan 2</Link></li>
+                  <li><Link to="/plan3">Plan 3</Link></li>
+                </ul>
+              </div>
+            )}
+          </li>
           <li><Link to="/client">CONOCE A MELINA</Link></li>
         </ul>
         <div className="extra-items">
